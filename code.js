325022e842d952904c0fc50c104fb98b10e3e209
seo-hyeonjeong <hyeons6499@gmail.com>
2021-03-30 11:12:44 +0900

    generate random quote and print message

--- code.js
+++ code.js
@@ -1,36 +1,35 @@
 const quotes = {
+    //quotes arranged into different moods
     _quotes: {
-        beforeExam: ["There is no substitute for hard work",
+        anxious: ["There is no substitute for hard work",
             "A quitter never wins - and a winner never quits",
             "Knowledge is power"],
-        uncertainty: ["True security lies in the unrestrained embrace of insecurity",
+        uncertain: ["True security lies in the unrestrained embrace of insecurity",
             "Happiness can be found even in the darkest of times",
             "Trust that there is a next step"],
         lazy: ["Do something today that your future self will thank you for",
             "Do it now", "Progress isn't made by early risers"]
     },
 
-    const generateRandNum = num => {
-        return Math.floor(Math.Random() * (num + 1));
+    //generates random number with max num-1
+    generateRandNum(num){
+        return Math.floor(Math.random() * (num));
     },
 
-    const countQuoteTypes = () => {
-        var length = 0;
-        for (var type in _quotes) {
-            if (_quotes.hasOwnProperty(type)) {
-                ++length;
-            }
-        }
-        return length;
+    //generate a random mood from _quotes and returns it in string format
+    generateRandType() {
+        var types = Object.keys(this._quotes);
+        var randType = types[this.generateRandNum(types.length)];
+        return randType;
     },
 
-    const generateQuote = () => {
-        var numOfTypes = countQuoteTypes();
-        //randomly select a type of quote (beforeExam, uncertainty, lazy). I thought doing _quotes.generateRandNum(numOfType) would work here but it doesnt because it is an object not an array. 
-        
-        
+    //randomly selects a quote from randtype
+    generateQuote() {
+        var type = this.generateRandType();
+        var resultQuote = this._quotes[type][this.generateRandNum(this._quotes[type].length)];
+        return `When you are ${type}, remember, '${resultQuote}'`;
     }
-
-
 }
 
+//print result
+console.log(quotes.generateQuote());
